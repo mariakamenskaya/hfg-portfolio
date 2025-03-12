cefffc6715bfe4d4cb2cbaa340c75c16e07da9ad
mariakamenskaya <152525722+mariakamenskaya@users.noreply.github.com>
2025-03-12 11:53:36 +0100

    Initial commit: Set up Nuxt.js project with Tailwind CSS, basic structure, and configuration files

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<Header />
+		<NuxtPage />
+		footer
+		<!-- <h2>Mein Portfolio</h2> -->
+	</div>
+</template>
+
+<script></script>
---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="fixed">
+		<NuxtLink to="/">Home</NuxtLink>
+		<NuxtLink to="/about">About</NuxtLink>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+a {
+	color: green;
+	padding-right: 20px;
+}
+</style>
